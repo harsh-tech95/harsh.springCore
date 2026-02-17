e49d2c424630f476a169e205c846d618b9a9c2f0
harsh-tech95 <mharshmishra39@gmail.com>
2026-02-17 09:58:02 +0530

    update

--- src/main/java/com/my/spring/Main.java
+++ src/main/java/com/my/spring/Main.java
@@ -1,8 +1,9 @@
 package com.my.spring;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.my.spring.model.Address;
 import com.my.spring.model.employee;
 
 public class Main {
@@ -10,10 +11,21 @@ public class Main {
 	public static void main(String[] args) {
 
 		
-		ApplicationContext ioc=new ClassPathXmlApplicationContext("application-context.xml");
+		ApplicationContext ioc=new AnnotationConfigApplicationContext("com.my.spring");
 employee bean=	ioc.getBean(employee.class);
 
+Address bean1=ioc.getBean(Address.class);
+
+bean1.setCity("Badaun");
+bean1.setState("UP");
+
+System.out.println(bean1);
+
+bean.setName("Arpit Kumar");
+bean.setId(101);
+bean.setGender("male");
 System.out.println(bean);
+
 	}
 
 }
